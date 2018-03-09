1477e3a6a45afd855b258585522ba35cf981aab5
Brian Haas <brianjohnhaas@gmail.com>
2018-03-08 22:11:47 -0500

    update

--- .__runMe.sh
+++ .__runMe.sh
@@ -1,4 +1,13 @@
+#!/bin/bash
+
+set -ex
+
 ../FusionFilter/prep_genome_lib.pl --genome_fa minichr2.fa --gtf minichr2.gtf --gmap_build
+
 ../../STAR-Fusion/STAR-Fusion --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz --genome_lib_dir ctat_genome_lib_build_dir/
-../DISCASM/DISCASM --chimeric_junctions STAR-Fusion_outdir/Chimeric.out.junction --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz --out_dir discasm --denovo_assembler Trinity
-../GMAP-fusion -T discasm/trinity_out_dir.Trinity.fasta --genome_lib_dir ctat_genome_lib_build_dir/ --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz
+
+../DISCASM/DISCASM --chimeric_junctions STAR-Fusion_outdir/Chimeric.out.junction --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz --out_dir discasm --denovo_assembler Trinity --aligned_bam  STAR-Fusion_outdir/std.STAR.bam
+
+../GMAP-fusion -T discasm/trinity_out_dir/Trinity.fasta --genome_lib_dir ctat_genome_lib_build_dir/ --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz -o gmapf
+
+centrifuge -x /seq/RNASEQ/TOOLS/CENTRIFUGE/data/p_compressed+h+v -f  -U discasm/trinity_out_dir/Trinity.fasta | tee centrifuge.txt
