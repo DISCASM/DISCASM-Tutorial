dbac7e1a48609d57839b639e20a85b56081e6bd1
brianjohnhaas <brianjohnhaas@gmail.com>
2019-06-02 10:53:48 +0000

    updated for compatibility w/latest software

--- .__runMe.sh
+++ .__runMe.sh
@@ -2,14 +2,16 @@
 
 set -ex
 
-../FusionFilter/prep_genome_lib.pl --genome_fa minichr2.fa --gtf minichr2.gtf --gmap_build
+../ctat-genome-lib-builder/prep_genome_lib.pl --genome_fa minichr2.fa --gtf minichr2.gtf --gmap_build
 
 ../../STAR-Fusion/STAR-Fusion --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz --genome_lib_dir ctat_genome_lib_build_dir/
 
-../DISCASM/DISCASM --chimeric_junctions STAR-Fusion_outdir/Chimeric.out.junction --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz --out_dir discasm --denovo_assembler Trinity --aligned_bam  STAR-Fusion_outdir/std.STAR.bam
+../DISCASM/DISCASM --chimeric_junctions STAR-Fusion_outdir/Chimeric.out.junction --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz --out_dir discasm --denovo_assembler Trinity --aligned_bam  STAR-Fusion_outdir/Aligned.out.bam
 
-../GMAP-fusion -T discasm/trinity_out_dir/Trinity.fasta --genome_lib_dir ctat_genome_lib_build_dir/ --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz -o gmapf
+../GMAP-fusion -T discasm/discasm.Trinity.fasta --genome_lib_dir ctat_genome_lib_build_dir/ --left_fq HCC1395-miniplus_1.fastq.gz --right_fq HCC1395-miniplus_2.fastq.gz -o gmapf
 
-cat  gmapf/GMAP-fusion.final
+cat  gmapf/GMAP-fusion.fusion_predictions.tsv
 
-centrifuge -x centrifuge_VirusDB/abv -f  -U discasm/trinity_out_dir/Trinity.fasta | tee centrifuge.matches.txt
+centrifuge -x centrifuge_VirusDB/abv -f  -U discasm/discasm.Trinity.fasta | tee centrifuge.matches.txt
+
+cat centrifuge_report.tsv
